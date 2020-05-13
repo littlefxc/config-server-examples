20fd6b9bd7274d5acb15ce95e98257bc077f6da0
littlefxc <12741723+littlefxc@users.noreply.github.com>
2020-05-13 15:46:45 +0800

    feat(application-nacos):添加apollo激活的namespaces信息输出

--- application-apollo/src/main/java/com/fengxuechao/example/application/AppInfoProperties.java
+++ application-apollo/src/main/java/com/fengxuechao/example/application/AppInfoProperties.java
@@ -1,5 +1,6 @@
 package com.fengxuechao.example.application;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
@@ -9,6 +10,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
  * @author fengxuechao
  * @date 2020/5/11
  */
+@Data
 @RefreshScope
 @ConfigurationProperties(prefix = "app.info")
 public class AppInfoProperties {
@@ -28,36 +30,8 @@ public class AppInfoProperties {
      */
     private String appVersion = "0.0.1-SNAPSHOT";
 
-    public String getConfigServer() {
-        return configServer;
-    }
-
-    public void setConfigServer(String configServer) {
-        this.configServer = configServer;
-    }
-
-    public String getAppName() {
-        return appName;
-    }
-
-    public void setAppName(String appName) {
-        this.appName = appName;
-    }
-
-    public String getAppVersion() {
-        return appVersion;
-    }
-
-    public void setAppVersion(String appVersion) {
-        this.appVersion = appVersion;
-    }
-
-    @Override
-    public String toString() {
-        return "AppInfoProperties{" +
-                "configServer='" + configServer + '\'' +
-                ", appName='" + appName + '\'' +
-                ", appVersion='" + appVersion + '\'' +
-                '}';
-    }
+    /**
+     * 激活的Apollo命名空间
+     */
+    private String apolloNameSpaces = "application";
 }
